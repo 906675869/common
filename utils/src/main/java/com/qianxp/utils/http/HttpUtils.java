d0198aac6ee136132680c08280dc95bcac495213
906675869 <906675869@qq.com>
2019-04-24 00:06:33 +0800

    http工具添加

---
+++ utils/src/main/java/com/qianxp/utils/http/HttpUtils.java
@@ -0,0 +1,117 @@
+package com.qianxp.utils.http;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpUtils{
+    private static Log log = LogFactory.getLog(HttpUtils.class);
+
+    public static String get(String url, Map<String, String> parmas, int retry) {
+        if (retry <= 0) {
+            log.error("connect timeout.cannot connect within time and the try times");
+            return null;
+        }
+        String result = null;
+        try {
+            result = get(url, parmas);
+        } catch (ConnectException cone) {
+            return get(url, parmas, --retry);
+        } catch (IOException ioe) {
+            log.error("request url fail,url :[{" + url + "}]", ioe);
+        }
+        return result;
+    }
+
+    public static String get(String url, Map<String, String> params) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String resultString = "";
+        CloseableHttpResponse response = null;
+        try {
+            // 创建uri
+            URIBuilder builder = new URIBuilder(url);
+            if (params != null) {
+                for (String key : params.keySet()) {
+                    builder.addParameter(key, params.get(key));
+                }
+            }
+            URI uri = builder.build();
+            // 创建http GET请求
+            HttpGet httpGet = new HttpGet(uri);
+            // 执行请求
+            response = httpClient.execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == 200) {
+                resultString = EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        } catch (URISyntaxException urie) {
+            log.error("URISyntax request fail. [{0}]", urie);
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return resultString;
+    }
+
+
+    public static String post(String url, Map<String, String> params) {
+        // 创建Httpclient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+        String resultString = "";
+        try {
+            // 创建Http Post请求
+            HttpPost httpPost = new HttpPost(url);
+            // 创建参数列表
+            if (params != null) {
+                List<NameValuePair> paramList = new ArrayList<>();
+                for (String key : params.keySet()) {
+                    paramList.add(new BasicNameValuePair(key, params.get(key)));
+                }
+                // 模拟表单
+                UrlEncodedFormEntity entity = new UrlEncodedFormEntity(paramList);
+                httpPost.setEntity(entity);
+            }
+            // 执行http请求
+            response = httpClient.execute(httpPost);
+            resultString = EntityUtils.toString(response.getEntity(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return resultString;
+    }
+
+
+}
